c561543d738240818110c83a552f638274e8e4cd
OguzkEREN <oguzkaganeren@gmail.com>
2018-03-12 20:21:36 +0300

    completed C++ int array

--- Arrays/C++/int/main.cpp
+++ Arrays/C++/int/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
+int sizeOfArray;
+void addElement(int array[],int element,int position){
+	array[position]=element;
+}
+void removeElement(int array[],int position){
+	array[position]=NULL;
+}
 int main(){
-	cout << "test";
+	sizeOfArray=10;
+	int myArray[sizeOfArray]={0};
+	addElement(myArray,5,0);
+	removeElement(myArray,0);
+	if(myArray[0]==NULL){
+		cout << "Yes, you can define NULL in integer array";
+	}
+	cout << myArray[0];
 	return 0;
 }
+
